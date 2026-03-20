734fba8b884958e5be41ee993c7c843fe73941db
wifihackx <wifihackx@gmail.com>
2026-03-20 01:56:51 +0100

    refactor(debug): reuse shared debug logger in utilities

--- src/js/logger-strategy.js
+++ src/js/logger-strategy.js
@@ -18,7 +18,11 @@ import {
 } from './logger-config.js';
 
 const debugLog = (...args) => {
-  if (window.__WFX_DEBUG__ === true) {
+  if (typeof window.__WFX_DEBUG_LOG__ === 'function') {
+    window.__WFX_DEBUG_LOG__(...args);
+    return;
+  }
+  if (window.__WIFIHACKX_DEBUG__ === true || window.__WFX_DEBUG__ === true) {
     console.info(...args);
   }
 };
